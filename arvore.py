003026d2b9598c9d0c23f10a8dfa73dbb59c0828
arquimago <thales@alvesnet.net>
2015-05-16 23:33:39 -0300

    tentando fazer árvore funcionar

--- arvore.py
+++ arvore.py
@@ -10,30 +10,36 @@ def imprimir(tabuleiro):
   print ' ' + tabuleiro[6] + ' | ' + tabuleiro[7] + ' | ' + tabuleiro[8]
   print '   |   |'
 
+def copiar(tab):
+  tabTemp = []
+  for celula in tab:
+    tabTemp+=celula
+  return tabTemp
+
 def rodar(tab):
   tabuleiro=[]
-  tabuleiro+=tab[3]
-  tabuleiro+=tab[6]
-  tabuleiro+=tab[9]
   tabuleiro+=tab[2]
   tabuleiro+=tab[5]
   tabuleiro+=tab[8]
   tabuleiro+=tab[1]
   tabuleiro+=tab[4]
   tabuleiro+=tab[7]
+  tabuleiro+=tab[0]
+  tabuleiro+=tab[3]
+  tabuleiro+=tab[6]
   return tabuleiro
 
 def espelhar(tab):
   tabuleiro=[]
-  tabuleiro+=tab[3]
   tabuleiro+=tab[2]
   tabuleiro+=tab[1]
-  tabuleiro+=tab[6]
+  tabuleiro+=tab[0]
   tabuleiro+=tab[5]
   tabuleiro+=tab[4]
-  tabuleiro+=tab[9]
+  tabuleiro+=tab[3]
   tabuleiro+=tab[8]
   tabuleiro+=tab[7]
+  tabuleiro+=tab[6]
   return tabuleiro  
 
 def igual(tab1,tab2):
@@ -53,10 +59,21 @@ def simetrico(tab1,tab2):
     tab2 = rodar(tab2)
   return False
 
-arvore = []
+def gerarArvore():
+  arvore=[]
+  tabuleiro = [' ']*9
+  tabuleiro[0]='X'
+  arvore+=[copiar(tabuleiro)]
+  tabuleiro[0]=' '
+  for i in range(1,9):
+    tabuleiro[i]='X'
+    aux=[]
+    for no in arvore:
+      print no
+      if not simetrico(no, tabuleiro):
+        aux+=[copiar(tabuleiro)]
+    tabuleiro[i]=' '
+    arvore+=aux
+  return arvore
 
-tabuleiro = [' ']*9
-
-tabTemp = tabuleiro
-
-imprimir(tabTemp)
+gerarArvore()
