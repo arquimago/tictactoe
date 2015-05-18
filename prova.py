dcc80cb03f23b3c6bee5ffb651f6bea8ac89913c
arquimago <thales@alvesnet.net>
2015-05-18 07:53:35 -0300

    arrumada impressão prova.py

--- prova.py
+++ prova.py
@@ -3,15 +3,20 @@ from minimax import minimax
 
 def main():
 
+  print "Terceira questao"
   tabuleiro = [' ']*9
-  
   raiz = arvore.No(tabuleiro)
-
+  print " "
   arvore.geraArvore(tabuleiro,raiz)
-
+  print "Abaixo segue a arvore, com nos raiz, filhos e netos"
+  print " "
+  arvore.imprimirArvore(raiz)
+  print " "
+  print "Avaliacao das folhas"
   arvore.avaliarArvore(raiz)
 
   print 'Minimax resultado: ' + str(minimax(raiz, 2, True))
+
 
 
 if __name__ == '__main__': main()
